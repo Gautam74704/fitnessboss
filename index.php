0d33f911a1ab2870c19a3929cf62a16ca86c150f
Gautam74704 <81484461+Gautam74704@users.noreply.github.com>
2021-03-27 23:24:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<link href="style.css" rel="stylesheet" type="text/css">
+    <title>FitnessBoss....!</title>
+  </head>
+  <body>
+	<header class="main_header">
+		<div class="d-flex justify-content-center align-items-center flex-column py-5">
+			<h1 class="main_heading">FitnessBoss</h1>
+			<p class="main_heading_para">Welcome to <span class="text-capitalize bg-dark text-white py-2 px-3">World Of Fitness</span></p>
+		</div>
+		<!-- The main images -->
+		<div class="main_header_div d-flex align-items-start justify-content-center pl-5 flex-column shadow">
+			<p style="font-size:4rem;">FitnessBoss</p>
+			<h2 class="animateword">
+				<span>This is a </span>
+				<div style="font-size:6rem;">
+					<ul class="flip">
+						<li>Fitness Affliated Website.</li> 
+						<li>Bodybuilding's Technical Information Site.</li>
+						<li>Fitness Products Review Site.</li>
+					</ul>
+				</div>
+			</h2>
+		</div>
+	</header>
+	<!-- header ends -->
+	<!-- two sided div start -->
+	<div class="container-fluid">
+		<div class="row">
+			<!-- right side work space -->
+			<div class="col-xl-10 col-lg-10 col-md-11 col-11 mx-auto my-5">
+				<div class="row gx-5 mx-sm-auto">
+					<!-- left side work space -->
+					<div class="col-lg-8 col-md-11 col-11 mx-auto">
+						<div class="row gy-5">
+							<div class="col-12 card p-4 shadow blog_left_div">
+								<center>
+								<div class="d-flex justify-content-center align-item-center flex-column pt-3 pd-5">
+									<h1>Chest Workout</h1>
+									<p class="blog_tittle"> <span class=font-weight-bold;>Title description, </span> feb 13,2021</p>
+								</div>
+								</center>
+								<figure class="right_side_img mb-5 ">
+									<img src="images/img1.jpg" class="img-fluid shadow">
+								</figure>
+								<p> <span class="font-weight-bold ">Chest Workout</span>
+								Be sure to have your pages set up with the latest design and development standards. That means using 
+								an HTML5 doctype and including a viewport meta tag for proper responsive behaviors. Put it all 
+								together and your pages should look like this:
+								</p>
+								<p>
+								Curious which components explicitly require our JavaScript and Popper? Click the show components link
+								below. If you’re at all unsure about the general page structure, keep reading for an example page 
+								template.
+								</p>
+							</div>
+							<div class="col-12 card p-4 shadow blog_left_div">
+								<center>
+								<div class="d-flex justify-content-center align-item-center flex-column pt-3 pd-5">
+									<h1>increase flexibility</h1>
+									<p class="blog_tittle"> <span class=font-weight-bold;>Title description, </span> feb 13,2021</p>
+								</div>
+								</center>
+								<figure class="right_side_img mb-5 ">
+									<img src="images/pexels-andrea-piacquadio-3764396.jpg" class="img-fluid shadow">
+								</figure>
+								<p> <span class="font-weight-bold ">Thapa Technical</span>
+								How Long to Hold Stretches to Increase Flexibility. In this kind of practice,
+								you're maintaining the posture long enough to affect the plasti
+								</p>
+								<p>
+								you can hold your current position and try to increase the range of motion every 
+								time you do. it. another way to
+								</p>
+							</div>
+							<div class="col-12 card p-4 shadow blog_left_div">
+								<center>
+								<div class="d-flex justify-content-center align-item-center flex-column pt-3 pd-5">
+									<h1>Ketogenic Diet</h1>
+									<p class="blog_tittle"> <span class=font-weight-bold;>Title description, </span> feb 13,2021</p>
+								</div>
+								</center>
+								<figure class="right_side_img mb-5 ">
+									<img src="images/pexels-natasha-spencer-2383009.jpg" class="img-fluid shadow">
+								</figure>
+								<p> <span class="font-weight-bold ">Ketogenic Diet</span>
+								The ketogenic diet is a very low carb, high fat diet that shares many similarities with the 
+								Atkins and low carb diets. It involves drastically reducing carbohydrate intake and replacing 
+								it with fat. This reduction in carbs puts your body into a metabolic state called ketosis.
+								</p>
+							</div> 
+						</div>
+					</div>
+					<!-- work on right side space -->
+					<div class="col-lg-3 col-md-7 col-11 justify-content-end m-lg-0 m-auto">
+						<div class="row gy-5 left_div_blog">
+						<!-- popular post -->
+							<div class="popular_post">
+								<div class="right_div_title py-4 pl-2">
+									<h2>Popular Post</h2>
+								</div>
+								<div class="right_sub_div shadow">
+									<div class="row">
+										<div class="col-4 popular_post_img1 py-2 pl-2">
+											<img src="images/OF3Zstl-bill-gates-wallpaper.jpg" class="img-fluid shadow"/>
+										</div>
+										<div class="col-8">
+											<h3>Bill Gates</h3>
+											<p>CEO of Microsoft</p>
+										</div>
+									</diV>
+									<div class="row">
+										<div class="col-4 popular_post_img1 py-2 pl-2">
+											<img src="images/i7RUTzb-steve-jobs-wallpaper.jpg" class="img-fluid shadow"/>
+										</div>
+										<div class="col-8">
+											<h3>Steve Jobs</h3>
+											<p>Founder of Apple</p>
+										</div>
+									</diV>
+									<div class="row">
+										<div class="col-4 popular_post_img1 py-2 pl-2" >
+											<img src="images/wp4025589-jeff-bezos-wallpapers.jpg" class="img-fluid shadow"/>
+										</div>
+										<div class="col-8">
+											<h3>Jeff Bezos</h3>
+											<p>CEO of Amazon</p>
+										</div>
+									</diV>
+								</div>
+							</div>
+							<!-- Avertisement -->
+							<div class="right_div_post">
+								<div class="right_div_title py-4 pl-2">
+									<h2>Avertise<h2>
+								</div>
+								<div class="right_sub_div">
+									<div class="advertise_img bg-light shadow">
+										<p>Ads goes here</p>
+									</div>
+								</div>
+							</div>
+							<!-- Inspiration -->
+							<div class="right_div_post">
+								<div class="right_div_title py-4 pl-2">
+									<h2>Diet</h2>
+								</div>
+								<div class="right_sub_div">
+									<div class="row gx-3">
+										<div class="col-6">
+											<figure>
+												<img src="images/pexels-markus-winkler-2862154.jpg" class="img-fluid shadow" />
+											</figure>
+										</div>
+										<div class="col-6">
+											<figure>
+												<img src="images/pexels-iina-luoto-1211887.jpg" class="img-fluid shadow"/>
+											</figure>
+										</div>
+										<div class="col-6">
+											<figure>
+												<img src="images/pexels-daria-shevtsova-949067.jpg" class="img-fluid shadow"/>
+											</figure>
+										</div>
+										<div class="col-6">
+											<figure>
+												<img src="images/pexels-alleksana-4113834.jpg" class="img-fluid shadow"/>
+											</figure>
+										</div>
+									</div>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer>
+		<p class="text-center py-4 bg-light">contact me:- gautamkumar74704@gmail.com</p>
+	</footer>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
+
+  </body>
+</html>
